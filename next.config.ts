fcb1208df8be8596cdbda48063098f5e58856b51
tranhoc22102005-max <tranhoc22102005@gmail.com>
2026-03-28 18:52:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const BASE_PATH = isProd ? "/portfolio" : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: BASE_PATH,
-  assetPrefix: isProd ? "/portfolio/" : "",
+  basePath: "",
+  assetPrefix: "",
   env: {
-    NEXT_PUBLIC_BASE_PATH: BASE_PATH,
+    NEXT_PUBLIC_BASE_PATH: "",
   },
   images: {
     unoptimized: true,
